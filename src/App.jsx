78e75dd3688d8b596d376d5ca93cdc6c3160101d
Milan Sharma <milan@Milans-MacBook-Pro.local>
2025-04-21 18:39:13 +0530

    initial setup of the project

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+// function App() {
+//   return (
+//     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+//       <h1 className="text-3xl font-bold text-pink-800">
+//         React with Tailwind CSS v4 is Running!
+//       </h1>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+import './App.css'
+import Layout from './Layout/Layout';
+
+
+
+function App() {
+
+  return (
+    <Layout />
+  );
+}
+
+export default App
+
