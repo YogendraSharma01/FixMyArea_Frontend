3bd3c426309799168ad576f47a21a9caf5326554
Milan Sharma <milan@Milans-MacBook-Pro.local>
2025-04-22 10:22:44 +0530

    resolved the conflict of the yogesh and milan code

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,3 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-
-// function App() {
-//   return (
-//     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-//       <h1 className="text-3xl font-bold text-pink-800">
-//         React with Tailwind CSS v4 is Running!
-//       </h1>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 
 
 import './App.css'
